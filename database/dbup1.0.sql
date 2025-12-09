35c5ffb92a8a88f3a80970baf7b101c3bf6bbc43
icalxxz <faishalcandra10@gmail.com>
2025-12-09 21:56:38 +0700

    first

---
+++ database/dbup1.0.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (id, uid, email, role, siswa_id, created_at)
+VALUES (
+    id:int,
+    'uid:varchar',
+    'email:varchar',
+    'role:enum',
+    'siswa_id:varchar',
+    'created_at:timestamp'
+  );
